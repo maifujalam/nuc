c33e7124ac04e79bdc453b3fb2b92d8381f4d182
skmaifujalam <sk.maifujalam@gmail.com>
2026-09-23 19:52:37 +0400

    nftables

--- shellscripts/secrets/aws_secret.sh
+++ shellscripts/secrets/aws_secret.sh
@@ -8,10 +8,10 @@ if [ -z "${AWS_ACCESS_KEY_ID:-}" ] || [ -z "${AWS_SECRET_ACCESS_KEY:-}" ];then
 fi
 
 
-kubectl -n jenkins delete secret aws-secret --ignore-not-found
-kubectl create ns jenkins 2>/dev/null || true
-kubectl -n jenkins create secret generic aws-secret --from-literal=AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
---from-literal=AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+#kubectl -n jenkins delete secret aws-secret --ignore-not-found
+#kubectl create ns jenkins 2>/dev/null || true
+#kubectl -n jenkins create secret generic aws-secret --from-literal=AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+#--from-literal=AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 
 kubectl -n cert-manager delete secret aws-credentials-secret --ignore-not-found
 kubectl create ns cert-manager 2>/dev/null || true
